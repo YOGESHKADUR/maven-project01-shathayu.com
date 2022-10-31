5d9f4e50094a4104da46419451e38f13105f7677
YOGESHKADUR <yogeshkadur@gmail.com>
2022-10-31 06:10:03 +0530

    3rd commit

---
+++ src/main/java/com/shathayu/demo_project/App.java
@@ -0,0 +1,13 @@
+package com.shathayu.demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
